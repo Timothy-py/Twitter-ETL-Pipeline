96dd64341bdb4b69f6a9178899d4ddba84711ff4
ADEYEYE TIMOTHY <adeyeyetimothy33@gmail.com>
2019-10-03 07:24:46 +0100

    Added documentation

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from tweepy import StreamListener, OAuthHandler, Stream
 import json
-from kafka import KafkaProducer, KafkaConsumer
+from kafka import KafkaProducer
+
 topic_name = 'timothy_twitter_stream_topic'
+
+# instantiate and configure the kafka producer class
 producer = KafkaProducer(
     bootstrap_servers='52.19.199.252:9092,52.19.199.252:9093,52.19.199.252:9094',
     value_serializer=lambda x: json.dumps(x).encode('utf-8')
@@ -12,7 +15,8 @@ producer = KafkaProducer(
 
 class Authenticator:
     """
-
+    this class authenticate the twitter app with the provided access key and
+    secret access token from twitter.
     """
 
     def __init__(self):
@@ -27,7 +31,8 @@ class Authenticator:
 
 class Streamer:
     """
-
+    this class is does the function of using the Authenticator class to get stream tweets from twitter
+    based on the provided keywords i.e track_list
     """
     def __init__(self):
         pass
@@ -40,13 +45,18 @@ class Streamer:
 
 class ListenerFunction(StreamListener):
     """
-
+    this class primarily performs the functionality displaying the streamed tweets from the Streamer class
+    and it is also triggered in the Streamer class
     """
 
     def on_data(self, data):
+
+        # the Data variable contains the streamed tweets(originally in json format) in python object i.e dict
         Data = json.loads(data)
         # stringData = json.dumps(Data, indent=2)
 
+        # this variable contains a formatted form of Data variable(which contain a lots of data), to get only
+        # the necessary fields.
         self.dataDict = {
             'text': Data['text'],
             'created_at': Data['created_at'],
@@ -60,6 +70,7 @@ class ListenerFunction(StreamListener):
             'source': Data['source']
         }
 
+        # push the formatted data(i.e dataDict) to kafka
         producer.send(topic_name, value=self.dataDict)
 
     def on_error(self, status):
@@ -69,7 +80,9 @@ class ListenerFunction(StreamListener):
 
 
 if __name__ == "__main__":
+    # the variable contain the keywords to search for in a tweet.
     track_list = ['Nigeria', 'Lagos', 'Buhari']
+    # execute the streamer class
     Streamer().stream()
 
 
--- consumer.py
+++ consumer.py
@@ -1,18 +1,24 @@
 from kafka import KafkaConsumer
-import time, json
+import time
+import json
+
 topic_name = 'timothy_twitter_stream_topic'
+
+# instantiate and configure the kafka consumer class
 consumer = KafkaConsumer(topic_name, bootstrap_servers='52.19.199.252:9092,52.19.199.252:9093,52.19.199.252:9094',
                          auto_offset_reset='earliest',
                          enable_auto_commit=False)
 
+# simple print statement to determine if the kafka consumer object is well set
 print('Consumer ready = {}'.format(consumer))
 
+# a json file with a time stamp to save each tweets
 fileObj = open('timothy_twitter_stream_topic_{}.json'.format(str(time.time())), 'w')
 
+# loop through all the data in kafka, and write each data into the timestamped json file.
 for message in consumer:
     jsonFile = json.dumps(message.value.decode('UTF-8'))
     fileObj.write(jsonFile)
-    # fileObj.close()
     print('Done!')
 
 
